56416f4d4ee5fc984ada8568110febf28592d8aa
Ahmet Taşcıoğlu <ahmettascioglu@outlook.com>
2021-07-10 14:04:39 +0300

    Add project files.

---
+++ Kaizen.CaseStudy.NewsSummary/Helper/TextReaderHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizen.CaseStudy.NewsSummary.Helper
+{
+    public class TextReaderHelper
+    {
+        public static string ReadTextFile(string path)
+        {
+            if (!System.IO.File.Exists(path))
+                throw new Exception("File does not exists");
+
+            using (TextReader reader = new StreamReader(System.IO.File.Open(path, FileMode.Open)))
+            {
+                try
+                {
+                    var newsContent = reader.ReadToEnd();
+                    return newsContent;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("File couldn't open and read");
+                }
+            }
+        }
+    }
+}
---
+++ Kaizen.CaseStudy.NewsSummary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kaizen.CaseStudy.NewsSummary.Helper;
+
+namespace Kaizen.CaseStudy.NewsSummary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = TextReaderHelper.ReadTextFile(@"C:\Users\ahmet\Downloads\news.txt");
+                string title = "Korona günlerinde arabalı sinema keyfi";
+
+                // I decided to split news title and get words. after getting them we will search words in main content to get summary
+
+                var titleWords = title.Split(" ");
+
+                // Split sentences with (dash) character
+                var sentencesOfContent = content.Split(".");
+
+                List<string> summarySentenceList = new List<string>();
+                foreach (var sentence in sentencesOfContent)
+                {
+                    foreach (var word in titleWords)
+                    {
+                        if (sentence.Contains(word))
+                        {
+                            if (!summarySentenceList.Any(x => x.Equals(sentence)))
+                            {
+                                summarySentenceList.Add(sentence);
+                            }
+                        }
+                    }
+                }
+
+                var summary = string.Join(".", summarySentenceList);
+
+                Console.Write(summary);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
